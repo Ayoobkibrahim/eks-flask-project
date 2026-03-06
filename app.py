4d6e35a950b5053997c4b789fac5826875ee4161
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2026-03-06 16:18:44 +0530

    change app content for testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"status": "ok", "message": "Hello From  EKS! CI/CD"})
+    return jsonify({"status": "ok", "message": "Hello From  EKS! to deploy flask app"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000    )
