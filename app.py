df58ef092ae8442b5fcd9b62df055f059b4aaf77
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2026-03-06 19:50:12 +0530

    modify terraform and k8s yaml files and display content in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"status": "ok", "message": " Hello This is My ESK + Flask Using project"})
+    return jsonify({"status": "ok", "message": " Hello This is My EKs + Flask Using project with Terraform"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000    )
