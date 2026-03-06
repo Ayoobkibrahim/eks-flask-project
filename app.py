d492152667299ce2a66c3947195e5d00ee6503f1
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2026-03-06 16:02:49 +0530

    change app content

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"status": "ok", "message": "Hello From  EKS!"})
+    return jsonify({"status": "ok", "message": "Hello From  EKS! CI/CD"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000    )
